860d12a55d1945f5e549818cf81c885f9476b210
SuperValou <valentin.esmieu@tuta.io>
2021-06-05 19:22:11 +0200

    Create gizmo for Trigger class

---
+++ Frigate/Assets/Scripts/Editor/Gizmos/TriggerGizmo.cs
@@ -0,0 +1,32 @@
+﻿using Assets.Scripts.Environments;
+using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Scripts.Editor.Gizmos
+{
+    public static class TriggerGizmo
+    {
+        private static readonly Color Color = new Color(r:0, g:1, b:0, a:0.1f);
+
+        [DrawGizmo(GizmoType.NonSelected)]
+        private static void DrawTriggerGizmo(Trigger trigger, GizmoType gizmoType)
+        {
+            UnityEngine.Gizmos.color = Color;
+
+            var col = trigger.GetComponent<Collider>();
+            if (col is BoxCollider box)
+            {
+                UnityEngine.Gizmos.DrawCube(box.transform.position, box.size);
+                return;
+            }
+
+            if (col is SphereCollider sphere)
+            {
+                UnityEngine.Gizmos.DrawSphere(sphere.transform.position, sphere.radius);
+                return;
+            }
+
+            UnityEngine.Gizmos.DrawWireCube(col.transform.position, col.bounds.size);
+        }
+    }
+}
