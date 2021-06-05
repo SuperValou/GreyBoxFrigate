caa9bd151b2d6d7785c5da5acb1ab4ef0b113d88
SuperValou <valentin.esmieu@tuta.io>
2021-06-05 19:39:53 +0200

    Add gizmo icons for LockOnTarget and Counter classes

---
+++ Frigate/Assets/Scripts/Editor/Gizmos/CounterGizmo.cs
@@ -0,0 +1,17 @@
+﻿using Assets.Scripts.Environments;
+using Assets.Scripts.Players.LockOns;
+using UnityEditor;
+
+namespace Assets.Scripts.Editor.Gizmos
+{
+    public class CounterGizmo
+    {
+        private const string IconName = "counter.png";
+
+        [DrawGizmo(GizmoType.NonSelected | GizmoType.Selected)]
+        private static void DrawTriggerGizmo(Counter counter, GizmoType gizmoType)
+        {
+            UnityEngine.Gizmos.DrawIcon(counter.transform.position, IconName, allowScaling: true);
+        }
+    }
+}
---
+++ Frigate/Assets/Scripts/Editor/Gizmos/LockOnTargetGizmo.cs
@@ -0,0 +1,18 @@
+﻿using Assets.Scripts.Environments;
+using Assets.Scripts.Players.LockOns;
+using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Scripts.Editor.Gizmos
+{
+    public static class LockOnTargetGizmo
+    {
+        private const string IconName = "target.png";
+
+        [DrawGizmo(GizmoType.NonSelected | GizmoType.Selected)]
+        private static void DrawTriggerGizmo(LockOnTarget target, GizmoType gizmoType)
+        {
+            UnityEngine.Gizmos.DrawIcon(target.transform.position, IconName, allowScaling: true);
+        }
+    }
+}
